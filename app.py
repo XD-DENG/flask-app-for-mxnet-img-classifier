13e6fe843daea45e9beb0d5e23832174a6369d28
Xiaodong Deng <deng_xiao_dong@manulife.com>
2017-07-11 09:26:05 +0800

    Refine the app.py a bit

--- app.py
+++ app.py
@@ -3,16 +3,26 @@ from werkzeug.utils import secure_filename
 import os
 import cv2
 import numpy as np
-# define a simple data batch
 from collections import namedtuple
 import mxnet as mx
 import  hashlib
 import datetime
 
-Batch = namedtuple('Batch', ['data'])
+
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 # restrict the size of the file uploaded
+# restrict the size of the file uploaded
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+
+
+
+
+################################################
+# Functions for running classifier
+################################################
+
+# define a simple data batch
+Batch = namedtuple('Batch', ['data'])
 
 # Prapare the MXNet model (pre-trained)
 sym, arg_params, aux_params = mx.model.load_checkpoint('resnet-152', 0)
@@ -24,44 +34,54 @@ with open('synset.txt', 'r') as f:
     labels = [l.rstrip() for l in f]
 
 
-
-def get_image(url, local=False):
-    # download and show the image
+def get_image(file_location, local=False):
+    # users can either 
+    # [1] upload a picture (local = True)
+    # or
+    # [2] provide the image URL (local = False)
     if local == True:
-        fname = url
+        fname = file_location
     else:
-        fname = mx.test_utils.download(url, dirname="static/img_pool")
+        fname = mx.test_utils.download(file_location, dirname="static/img_pool")
     img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
-
-    #os.remove(fname)
 
     if img is None:
          return None
+    
     # convert into format (batch, RGB, width, height)
     img = cv2.resize(img, (224, 224))
     img = np.swapaxes(img, 0, 2)
     img = np.swapaxes(img, 1, 2)
     img = img[np.newaxis, :]
+
     return img
 
-def mx_predict(url, local=False):
-    img = get_image(url, local)
+
+def mx_predict(file_location, local=False):
+    img = get_image(file_location, local)
+
     # compute the predict probabilities
     mod.forward(Batch([mx.nd.array(img)]))
     prob = mod.get_outputs()[0].asnumpy()
-    # print the top-5
+
+    # Return the top-5
     prob = np.squeeze(prob)
     a = np.argsort(prob)[::-1]
     result = []
     for i in a[0:5]:
         result.append((labels[i].split(" ", 1)[1], round(prob[i], 3)))
+
     return result
 
 
-
+################################################
+# Functions Building Endpoints
+################################################
 
 @app.route("/", methods = ['POST', "GET"])
 def FUN_root():
+	# Run correspoing code when the user provides the image url
+	# If user chooses to upload an image instead, endpoint "/upload_image" will be invoked
     if request.method == "POST":
         img_url = request.form.get("img_url")
         prediction_result = mx_predict(img_url)
@@ -71,11 +91,9 @@ def FUN_root():
         return render_template("index.html")
 
 
-
 @app.route("/about/")
 def FUN_about():
     return render_template("about.html")
-
 
 
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'bmp']
@@ -91,9 +109,11 @@ def FUN_upload_image():
         if 'file' not in request.files:
             return(redirect(url_for("FUN_root")))
         file = request.files['file']
+
         # if user does not select file, browser also submit a empty part without filename
         if file.filename == '':
             return(redirect(url_for("FUN_root")))
+
         if file and allowed_file(file.filename):
             filename = os.path.join("static/img_pool", hashlib.sha256(str(datetime.datetime.now())).hexdigest() + secure_filename(file.filename).lower())
             file.save(filename)
@@ -102,6 +122,8 @@ def FUN_upload_image():
     return(redirect(url_for("FUN_root")))
 
 
-
+################################################
+# Start the service
+################################################
 if __name__ == "__main__":
     app.run(debug=True)
